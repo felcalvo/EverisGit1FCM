f230bf37c78fd96e3a7880284d90df630ad45e02
f.calvo <fernando_calvo@icloud.com>
2021-10-25 13:54:57 +0200

    Modificado de nuevo el saludo

--- maven-archetype-quickstart/src/main/java/com/everis/App.java
+++ maven-archetype-quickstart/src/main/java/com/everis/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola, Soy Fernando Calvo. Vivo en la provincia de Huelva!!!!!!!!" );
+        System.out.println( "Hola, Soy Fernando Calvo. Vivo en la provincia de Huelva y deseo obtener un puesto de trabajo en NTT Data ;-)" );
     }
 }
